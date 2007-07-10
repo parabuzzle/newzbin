2d1d90dab4207488984cd05d79098335549200a1
jmaddox <jmaddox@8104e683-151a-0410-92e0-d8f0e6fb3da3>
2007-07-10 07:36:37 +0000

    add test file
    git-svn-id: http://code.jtoe.net/ruby/newzbin@747 8104e683-151a-0410-92e0-d8f0e6fb3da3

---
+++ test.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+#
+#  Created by Jon Maddox on 2007-02-10.
+#  Copyright (c) 2007. All rights reserved.
+
+require 'newzbin'
+
+newz = Newzbin::Connection.new('seven5', 'twan7575')
+nzbs = newz.search(:q => 'zodiac')
+
+puts nzbs.inspect
+
+new_attr = {}
+
+nzbs.first.attributes.each_pair do |key, value|
+  puts "#{key}: #{value}"
+end
+
+
+
+#newz.get_nzb(nzbs.first.id)
+
+
+# http://v3.newzbin.com/search/?q=casino+royale&searchaction=Search&fpn=p&category=-1&area=-1&u_nfo_posts_only=0&u_url_posts_only=0&u_comment_posts_only=0&u_v3_retention=5184000&ps_rb_source=64&ps_rb_video_format=16&sort=ps_edit_date&order=desc&areadone=-1&feed=rss&fauth=MjIwNTk1LTJkZWM4Yjk3NGU4Mjg2ODc2ODRiZmI5ZWVkMjE2NjNkNTIwMDA4MmI%3D
+# http://v3.newzbin.com/search/?q=casino+royale&searchaction=Search&fpn=p&category=-1&area=-1&u_nfo_posts_only=0&u_url_posts_only=0&u_comment_posts_only=0&u_v3_retention=5184000                &ps_rb_video_format=13&sort=ps_edit_date&order=desc&areadone=-1&feed=rss&fauth=MjIwNTk1LTZmMmM2ZmI3Y2NiOWQwYjJlNDEyMWVhYTU2ZDEyMWE2ZjY4ZTQ1ZDk%3D
+
+# q=casino+royale
+# searchaction=Search
+# fpn=p
+# category=-1
+# area=-1
+# u_nfo_posts_only=0
+# u_url_posts_only=0
+# u_comment_posts_only=0
+# u_v3_retention=5184000
+# ps_rb_video_format=131072
+# sort=ps_edit_date
+# order=desc
+# areadone=-1
+# feed=rss
+# fauth=MjIwNTk1LTZmMmM2ZmI3Y2NiOWQwYjJlNDEyMWVhYTU2ZDEyMWE2ZjY4ZTQ1ZDk%3D
