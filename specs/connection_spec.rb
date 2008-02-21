060521516c9538f67fa717d39131c72c0be668b0
Jon Maddox <jon@jonsthoughtsoneverything.com>
2008-02-21 09:37:20 -0500

    more specs

--- specs/connection_spec.rb
+++ specs/connection_spec.rb
@@ -53,4 +53,45 @@ describe Connection, "when first creating with username and pass" do
 
 end
 
+describe Connection, "when doing a search without cookies" do
+  before(:each) do
+    newzbin_conn = Connection.new
+    @nzbs = newzbin_conn.search(:q => 'independence day', :ps_rb_video_format => 16, "category"=>"6", "commit"=>"search")
+  end
+
+  it "should return an array of items" do
+    @nzbs.class.should be(Array)
+  end
+
+  it "should return items" do
+    @nzbs.size.should have_at_least(1).things
+  end
+
+  it "should get attributes for nzbs" do
+    @nzbs.first.attributes.size.should eql(0)
+  end
+
+end
+
+describe Connection, "when doing a search with cookies" do
+  before(:each) do
+    newzbin_conn = Connection.new(:nzbSmoke => "RKTU0McXx%24Uc4e4KXP1L0sl4O1U9YOchO%2B0DA%3D", :nzbSessionID => "a0fed567eb1a3e6e95c8a3d46fe0c6e7")
+    @nzbs = newzbin_conn.search(:q => 'independence day', :ps_rb_video_format => 16, "category"=>"6", "commit"=>"search")
+  end
+
+  it "should return an array of items" do
+    @nzbs.class.should be(Array)
+  end
+
+  it "should return items" do
+    @nzbs.size.should have_at_least(1).things
+  end
+
+  it "should get attributes for nzbs" do
+    @nzbs.first.attributes.size.should have_at_least(1).things
+  end
+
+end
+
+
 
